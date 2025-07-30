6af56e43b31a2cd905059722bdad16a1d4006f9e
Nesheen <bianchineyen@gmail.com>
2025-07-30 14:01:34 -0300

    Add files via upload

---
+++ Introduccion_Java/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int edad=0;
+
+        System.out.println("Ingrese su edad: ");
+
+        if(scanner.hasNextInt()){
+           edad = scanner.nextInt();
+        }else{
+            System.out.println("Ingrese la edad en forma de entero");
+            return;
+        }
+        if (edad >=18){
+            System.out.println("Usted es mayor de edad");
+        }else{
+            System.out.println("Usted es menor de edad");
+        }
+    }
+}
